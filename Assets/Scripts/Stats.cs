78dfebd0164475224c1a9dba0b59ca1668d5b908
Tejas Raghuvanshi <tejasraghuvanshi23@gmail.com>
2024-11-16 18:17:02 -0500

    stats added

---
+++ Assets/Scripts/Stats.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Stats : MonoBehaviour
+{
+    public int health = 100;
+    public int maxHealth = 100;
+    public int range = 5;
+    public int speed = 20;
+    public int damage = 50;
+    
+    
+    void Start()
+    {
+        
+    }
+    
+    void Update()
+    {
+        
+    }
+}
