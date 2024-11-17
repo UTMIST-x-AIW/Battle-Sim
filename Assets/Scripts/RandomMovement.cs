40993cf0be458a6e28b33532b2cd625e7b63f897
Kushagra Raghuvanshi <kushagra2125@gmail.com>
2024-11-16 21:13:12 -0500

    players with corrected random movement

--- Assets/Scripts/RandomMovement.cs
+++ Assets/Scripts/RandomMovement.cs
@@ -6,7 +6,7 @@ using Random = UnityEngine.Random;
 
 public class RandomMovement : MonoBehaviour
 {
-    private float moveSpeed = 20.0f;
+    private float moveSpeed = 200.0f;
     private float changeInterval = 1.0f;
 
     private Vector2 randomDirection;
@@ -28,16 +28,20 @@ public class RandomMovement : MonoBehaviour
         }
     }
 
-    private void GenerateNewDirection()
+    void GenerateNewDirection()
     {
-        randomDirection = new Vector3(Random.Range(-1f, 1f), Random.Range(-1f, 1f), 0f).normalized;
+        // Generate a random angle in radians
+        float randomAngle = Random.Range(0f, Mathf.PI * 2);
+
+        // Convert angle to a direction vector on the unit circle
+        randomDirection = new Vector3(Mathf.Cos(randomAngle), Mathf.Sin(randomAngle), 0f);
     }
 
-    private void OnCollisionEnter2D(Collision2D other)
+    private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.gameObject.tag == "bounds")
         {
-            Debug.Log("Hit Bound");
+            Debug.Log("Hit Bound trigger");
             randomDirection = -1 * randomDirection;
         }
     }
