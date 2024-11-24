fc7f47f1209a6f53c8477558a740a806b73463f4
JTR781888 <mohamedsama1212@gmail.com>
2024-11-23 20:20:31 -0500

    Added the spawn and death animations

--- Assets/Scripts/GameObjectClass.cs
+++ Assets/Scripts/GameObjectClass.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -11,8 +12,22 @@ public class GameObjectClass : MonoBehaviour
         Tree,
         Rock
     }
-
+    public Animator animator {get; private set;}
+    
     private int HP;
+    
+    public void Start() 
+    {
+        HP = this.HP;
+        animator = GetComponent<Animator>();
+    }
+
+    public void Update()
+    {
+        
+    }
+
+    
 
     public virtual void TakeDamage(int damage)
     {
@@ -23,34 +38,60 @@ public class GameObjectClass : MonoBehaviour
     {
         
     }
-    
+
+    #region Inventory Implementation
     // A dictionary to store item names and their quantities
-    private Dictionary<string, int> Items = new Dictionary<string, int>();
+    private Dictionary<string, int> inventoryItems = new Dictionary<string, int>();
 
     // Add an item to the inventory
     public void AddItem(string itemName, int quantity = 1)
     {
-        if (Items.ContainsKey(itemName))
+        if (inventoryItems.ContainsKey(itemName))
         {
-            Items[itemName] += quantity;
+            inventoryItems[itemName] += quantity;
         }
         else
         {
-            Items[itemName] = quantity;
+            inventoryItems[itemName] = quantity;
         }
     }
 
     // Remove an item from the inventory
     public void RemoveItem(string itemName, int quantity = 1)
     {
-        if (Items.ContainsKey(itemName))
+        if (inventoryItems.ContainsKey(itemName))
         {
-            Items[itemName] -= quantity;
-            if (Items[itemName] <= 0)
+            inventoryItems[itemName] -= quantity;
+            if (inventoryItems[itemName] <= 0)
             {
-                Items.Remove(itemName);
+                inventoryItems.Remove(itemName);
             }
         }
     }
+    
 
+    #endregion
+    
+
+    #region Spawning Animation
+    
+    void PalyingSpawning()
+    {
+        animator.Play("Spawn"); // Play the spawn animation of the object
+        // Each of the gameobjects will have a different spawn animation that will be named Spawn
+    }
+    #endregion
+    #region Death Animation
+    void PalyingDeath()
+    {
+        animator.Play("Death"); // Play the death animation of the object
+        // Each of the gameobjects will have a different death animation that will be named Death
+    }
+     // At the end of the death animation clip in unity, there will be an animation event on the last frame
+     // The event will trigger this function
+     void OnDeathAnimation()
+     {
+         Destroy(this.gameObject);
+     }
+    #endregion
 }
