e9abfeb4172fd3007a4664883a3f27a4e542e288
JTR781888 <mohamedsama1212@gmail.com>
2025-03-15 22:22:39 -0400

    Added animation for albert

--- Assets/Scripts/DebugMovement.cs
+++ Assets/Scripts/DebugMovement.cs
@@ -3,89 +3,39 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.TestTools;
 using UnityEngine.Tilemaps;
+//using static UnityEditor.Searcher.SearcherWindow.Alignment;
 
 public class DebugMovement : MonoBehaviour
 {
     private float _vInput;
     private float _hInput;
+    private Animator _anim;
 
     [SerializeField]
     float MoveSpeed = 5f;
 
     public Vector2 lastdirection;
-    //private Dictionary<Vector2Int, float> elevation = new Dictionary<Vector2Int, float>();
-    //public float z = 0;
-    //public float GroundHeight;
-    //public Vector3 velocity;
-    //public float gravity = 9.8f;
-    //public Tilemap Tilemap;
-    //Transform character;
-
-    //private void Awake()
-    //{
-    //    //LoadTileMapElevation(Tilemap);
-    //    character = this.GetComponent<Transform>();
-    //    z = transform.position.z;
-    //}
-
+    private void OnEnable()
+    {
+        _anim = GetComponent<Animator>();
+    }
     // Update is called once per frame
     void Update()
     {
-        //z = transform.position.y;
         Vector2 movementdir = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
 
         lastdirection = movementdir;
         _vInput = Input.GetAxis("Vertical") * MoveSpeed;
         _hInput = Input.GetAxis("Horizontal") * MoveSpeed;
-        this.transform.Translate(Vector3.up * _vInput * Time.deltaTime);
-        this.transform.Translate(Vector3.right * _hInput * Time.deltaTime);
-        //Rigidbody2D rb = this.GetComponent<Rigidbody2D>();
-        //rb.MovePosition(rb.position + movementdir * MoveSpeed * Time.fixedDeltaTime);
-        //velocity = (Vector3.right * _hInput * Time.deltaTime + Vector3.up * _vInput * Time.deltaTime).normalized;
-        //GravitySim();
+        //this.transform.Translate(Vector3.up * _vInput * Time.deltaTime);
+        //this.transform.Translate(Vector3.right * _hInput * Time.deltaTime);
+        Vector2 movement = new Vector2(
+        _hInput - _vInput,   
+            (_hInput + _vInput) / 2 ).normalized;
+        this.transform.Translate(movement*Time.deltaTime);
+        _anim.SetFloat("MoveX", movement.x);
+        _anim.SetFloat("MoveY", movement.y);
+        _anim.SetBool("IsMoving", movement.sqrMagnitude > 0);
     }
-// Wanted to simulate gravity; I will make do it another day
-    //void GravitySim()
-    //{
-    //    if (elevation.ContainsKey(new Vector2Int((int)transform.position.x, (int)transform.position.y)))
-    //    {
-    //        GroundHeight = elevation[new Vector2Int((int)transform.position.x, (int)transform.position.y)];
-    //    }
-    //    else
-    //    {
-    //        GroundHeight = 4;
-    //    }
-    //    if (z > GroundHeight)
-    //    {
-    //        z -= gravity * Time.deltaTime;  // Fall down
-    //        this.transform.Translate(Vector3.down * Time.deltaTime);
-    //    }
-    //    else
-    //    {
-    //        z = GroundHeight;  // Snap to ground
-    //        //character.position.z = GroundHeight;
-    //    }
-    //}
 
-    //void LoadTileMapElevation(Tilemap tilemap)
-    //{
-    //    foreach (Vector3Int position in tilemap.cellBounds.allPositionsWithin)
-    //    {
-    //        if (tilemap.HasTile(position))
-    //        {
-    //            Vector2Int gridPos = new Vector2Int(position.x, position.y);
-    //            float tileElevation = GetHeightFromTilemap(tilemap);
-    //            if (!elevation.ContainsKey(gridPos))
-    //            {
-    //                elevation.Add(gridPos, tileElevation);
-    //            }
-
-    //        }
-    //    }
-    //}
-    //float GetHeightFromTilemap(Tilemap tilemap)
-    //{
-    //    if (tilemap.name.Contains("Ground")) return 1.0f;
-    //    return 0.0f;
-    //}
 }
