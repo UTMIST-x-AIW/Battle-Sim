f6935151e1c6fe230b22560e6a4d75210f4d3ef5
Ashish Ajin Thomas <Coolioboss7@gmail.com>
2025-06-06 05:32:00 +0200

    Opacity from DOTween Fiix

--- Assets/Scripts/Interactable.cs
+++ Assets/Scripts/Interactable.cs
@@ -37,7 +37,7 @@ public abstract class Interactable : MonoBehaviour
 
         if (renderer != null)
         {
-            renderer.color = originalColor;
+            renderer.color = Color.white;
         }
     }
 
